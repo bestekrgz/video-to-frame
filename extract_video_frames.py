71615f2c2c90c0e4acd521e7cf8ac5bbac5fc2ed
Beste Karagoz <bestekaragz@gmail.com>
2022-01-18 16:42:56 +0300

    add extract frame options

---
+++ extract_video_frames.py
@@ -0,0 +1,11 @@
+import cv2
+
+video = cv2.VideoCapture("example.mp4")
+success, frame = video.read()
+
+count = 1
+while success:
+    cv2.imwrite(f'images/{count}.jpg', frame)
+    success, frame = video.read()
+
+    count += 1
